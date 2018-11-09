2b08e1dcf053fc1404691bf4eae357ed00d42b5f
mrsuman2002 <sumanadhikari04@gmail.com>
2018-11-09 10:43:19 -0600

    Committing CodeChat_Extension folder for the first time

---
+++ CodeChat_Extension/extension.js
@@ -0,0 +1,43 @@
+
+// Entension for the CodeChat
+// ==========================
+const vscode = require('vscode');
+var tools = require('../Thrift/JavaScript_Client/Client');
+var subscription;
+var panel;
+function activate(context) {
+    console.log('Congratulations, your extension "CodeChat" is now active!');
+    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+        vscode.window.showInformationMessage('Hello Codechat');
+        // Create and show panel
+        // ---------------------
+        panel = vscode.window.createWebviewPanel('CodeChat', "CodeChat", vscode.ViewColumn.One, { });
+        // Calling function from Client.js named myfunc
+        // --------------------------------------------
+        tools.Clientfunc(panel.webview);
+    });
+
+    context.subscriptions.push(disposable);
+    // Taking the event from webview so that it can be used in onDidChangeTextDocument
+    // ---------------------------------------------------------------------------------
+    var listener = function(event) {
+        console.log("It happened", event);
+        tools.Clientfunc(panel.webview);
+    };
+    // Starts listening for the `event <https://code.visualstudio.com/docs/extensionAPI/vscode-api#Event>`_.
+    // ---------------------------------------------------------------------------------------------------------
+    subscription = vscode.workspace.onDidChangeTextDocument(listener);
+}
+
+exports.activate = activate;
+function deactivate() {
+    subscription.dispose(); // stop listening
+}
+exports.deactivate = deactivate;
+
+
+
+
+
+
+
