ff1bad9e7527fc0368e4173034f710697a1b3dd2
mrsuman2002 <sumanadhikari04@gmail.com>
2019-08-28 10:07:05 -0500

    updated

--- CodeChat_Extension/JavaScript_Client/Client.js
+++ CodeChat_Extension/JavaScript_Client/Client.js
@@ -7,9 +7,6 @@ var CodechatSyc = require('./gen-nodejs/CodechatSyc');
 const vscode = require('vscode');
 const assert = require('assert');
 
-
-
-
 // Creating a function name myfunc to import it in `../../CodeChat_Extension/extension.js`
 // -----------------------------------------------------------------------------------------
 
