6745c78a624e46b2573625b3e6086c6125209534
Bryan A. Jones <bjones@ece.msstate.edu>
2020-06-20 22:49:41 -0500

    Release: Bump version.

--- CodeChat_Server/CodeChat_Server/__init__.py
+++ CodeChat_Server/CodeChat_Server/__init__.py
@@ -4,4 +4,4 @@
 # Give the version number, which is read by `../setup.py` during packaging.
 # This is chosen following the `version convention
 # <http://packaging.python.org/en/latest/tutorial.html#version>`_.
-__version__ = "0.0.1"
+__version__ = "0.0.2"
