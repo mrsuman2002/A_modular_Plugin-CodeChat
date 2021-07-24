b15e2e4a5c62e95c1708fb6f46180800eb1839d8
bjones1 <bjones@ece.msstate.edu>
2021-07-24 23:26:26 +0100

    Add: mkdocs, Javadoc templates.

---
+++ CodeChat_Server/templates/javadoc/Simple.java
@@ -0,0 +1,13 @@
+/**
+* Say hello to Java.
+*/
+public class Simple{
+    /**
+     * Write the actual message.
+     *
+     * @param args The command-line arguments, which are ignored.
+     */
+    public static void main(String args[]) {
+        System.out.println("Hello Java");
+    }
+}
