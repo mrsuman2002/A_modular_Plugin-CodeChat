c9321e8b9906de9c436b451abee9874ce8c9c2f8
Peter Loux <prl83@msstate.edu>
2022-04-13 21:02:14 -0500

    put correct script in correct place

--- CodeChat_Server/codechat.sh
+++ CodeChat_Server/codechat.sh
@@ -7,13 +7,6 @@
 # then run:
 # $ bash install.sh
 
-# https://stackoverflow.com/questions/6141581/detect-python-version-in-shell-script
-
-# check for python
-if ! hash python; then
-    echo "Python is not installed"
-    exit 1
-fi
 
 # todo: if no python, echo commands to install
 
@@ -26,11 +19,8 @@ python3 -m pip install --user --upgrade pip
 python3 -m venv codechat
 
 # activate virtual environment
-source codechat/bin/activate
-# . codechat/bin/activate  (alternate command?)
 
-# check to see if CodeChat is already installed
-# todo: check version number
+
 
 if [[ -d codechat ]]; then
     echo "CodeChat already installed"
@@ -40,3 +30,5 @@ fi
 
 # echo server directory to user
 which CodeChat_Server
+
+
