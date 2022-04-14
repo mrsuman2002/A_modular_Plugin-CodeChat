3bfb32f98ec745b9c7e3f7ffeaa0c57731f8e4f8
JoeKenn18 <jdk398@msstate.edu>
2022-04-14 03:15:23 -0500

    Added more output to user to let them know what is going on.

--- CodeChat_Server/windows_installer.ps1
+++ CodeChat_Server/windows_installer.ps1
@@ -14,14 +14,17 @@ set-executionpolicy remotesigned
 # https://adamtheautomator.com/powershell-if-else/#:~:text=1%20The%20if%20statement%20contains%20the%20first%20test,if%20all%20the%20prior%20conditions%20tested%20are%20false.
 # 
 # 
-# code sample::
-# 
+# Automatically moves Powershell to the user directory, works better when using powershell as an admin
+cd $env:USERPROFILE
+
 $pythonVersion = python --version
 $pythonVersionReq = #whatever this needs to be
 if([string]::IsNullOrEmpty($pythonVersion)){
     $response = Read-Host -Prompt "You do not have Python installed, would you like to install the required version ($pythonVersion) (y/n)?"
     if($response == "y"){
-        # install Python
+        # echo "Installing Python Version ($pythonVersionReq)"
+        # install Python using Invoke-WebRequest?
+        echo "Not Implemented yet, please install python version $pythonVersionReq or later"
     }
     [else{
         echo "Please install the required version of python: $pythonVersionReq"
@@ -31,8 +34,9 @@ if([string]::IsNullOrEmpty($pythonVersion)){
 [elseif ($pythonVersion != $pythonVersionReq){
     $response = Read-Host -Prompt "You have Python version ($pythonVersion) installed, would you like to install the required version ($pythonVersion) (y/n)?"
     if($response == "y"){
-        echo "Installing Python Version ($pythonVersionReq)"
+        # echo "Installing Python Version ($pythonVersionReq)"
         # install Python
+        echo "Not Implemented yet, please install python version $pythonVersionReq or later"
     }
     [else{
         echo "Please install the required version of python: $pythonVersionReq"
@@ -48,19 +52,43 @@ if([string]::IsNullOrEmpty($pythonVersion)){
 #   $Answer = Read-Host -Prompt "You have Python version '$PyVers' Would you like to update your Python version?"
 
 # Make sure pip, the Python installer, is up to date on Windows
+echo "Upgrading pip..."
 py -m pip install --upgrade pip
+echo "Upgrade Sucessful"
 
-# Create a virtual enviroment named codechat
-py -m venv codechat
+echo "Creating codechat venv..."
+Get-Item codechat
+if([string]::IsNullOrEmpty($pythonVersion)){
+    # Create a virtual enviroment named codechat
+    py -m venv codechat
+    echo "Virtual Enviroment Created Sucessfully"
+}
+[else{
+    echo "codechat virtual enviroment already found skipping this step"
+}]
 
+echo "Activating Virutal enviroment"
 # Activate the virtual enviroment
 .\codechat\Scripts\activate
+echo "Virtual Enviroment Activated"
 
-# Install the CodeChat Server
-py -m pip install --upgrade CodeChat_Server
+# find CodeChat_Server.exe and tell user if just updating or installing
+Get-Command CodeChat_Server
 
-# Copies path of CodeChat_Server to the clipboard for easy pasting and displays path in terminal
-#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/set-clipboard?view=powershell-7.2
+if([string]::IsNullOrEmpty($pythonVersion)){
+    # Install the CodeChat Server
+    echo "installing CodeChat_Server"
+    py -m pip install --upgrade CodeChat_Server
+    echo "codechat Server Sucessfully Installed"
+}
+[else{
+    echo "CodeChat_Server found, running update"
+    py -m pip install --upgrade CodeChat_Server
+    echo "CodeChat_Server sucessfully updated"
+}]
+
+
+# `Set-Clipboard <https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/set-clipboard?view=powershell-7.2>`_: Copies path of CodeChat_Server to the clipboard for easy pasting and displays path in terminal
 $pathToCodeChat = Get-Command CodeChat_Server | Select-Object -ExpandProperty Definition
 Set-Clipboard $pathToCodeChat
 echo "Here is your path to CodeChat (Also copied to your clipboard): $pathToCodeChat"
