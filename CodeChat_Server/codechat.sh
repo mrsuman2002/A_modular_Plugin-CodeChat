41191dc0c1ce5c01733990f0a8e3901c269c551e
Peter Loux <prl83@msstate.edu>
2022-04-13 21:54:52 -0500

    improve linux install script

--- CodeChat_Server/codechat.sh
+++ CodeChat_Server/codechat.sh
@@ -3,17 +3,22 @@
 
 # Run (without the pound or dollar signs):
 
-# wget https://raw.githubusercontent.com/JoeKenn1118/CodeChat_system/master/CodeChat_Server/codechat.sh
-# bash codechat.sh
+# wget https://raw.githubusercontent.com/JoeKenn1118/CodeChat_system/master/CodeChat_Server/codechat.sh && bash codechat.sh
 
+
+# check for Python 3
 if ! hash python3; then
     echo "Python 3 is not installed"
     exit 1
 fi
 
+# check python version
+python3 -c 'import sys'
 
-# todo: check python version
-python3 -c 'import sys; print(sys.version_info[:])'
+if ! python3 -c 'import sys; assert sys.version_info >= (3,6)' > /dev/null; then
+    echo "Upgrade Python to version 3.6 or above"
+    exit 1
+fi
 
 # install / upgrade pip
 python3 -m pip install --user --upgrade pip
@@ -23,9 +28,11 @@ python3 -m venv codechat
 
 
 if [[ -d codechat ]]; then
-    echo "CodeChat already installed"
+    # echo "CodeChat already installed"
+    codechat/bin/CodeChat_Server serve
 else
-    python3 -m pip install --upgrade CodeChat_Server
+    codechat/bin/python3 -m pip install --upgrade CodeChat_Server
+    codechat/bin/CodeChat_Server serve
 fi
 
 # echo server directory to user
