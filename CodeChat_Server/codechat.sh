76259d4a216b7241fdd0cc5844155d282f2cab03
Peter Loux <prl83@msstate.edu>
2022-04-18 16:08:41 -0500

    complete linux install script

--- CodeChat_Server/codechat.sh
+++ CodeChat_Server/codechat.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# installs CodeChat
 
-# Run (without the pound or dollar signs):
+# Installs CodeChat
 
-# wget https://raw.githubusercontent.com/JoeKenn1118/CodeChat_system/master/CodeChat_Server/codechat.sh && bash codechat.sh
-
+# To download this script and automatically run it, run in a terminal (without the # or $ signs):
+# $ wget https://raw.githubusercontent.com/JoeKenn1118/CodeChat_system/master/CodeChat_Server/codechat.sh && bash codechat.sh
 
 # check for Python 3
 if ! hash python3; then
@@ -15,37 +14,28 @@ fi
 # check python version
 python3 -c 'import sys'
 
-## redirect to stderr
-
-if ! python3 -c 'import sys; assert sys.version_info >= (4,6)' 2> /dev/null; then
-    echo "Upgrade Python to version 3.6 or above"
+if ! python3 -c 'import sys; assert sys.version_info >= (3,7)' 2> /dev/null; then
+    echo "Upgrade Python to version 3.7 or above"
     exit 1
 fi
 
 # install / upgrade pip
 python3 -m pip install --user --upgrade pip
 
-# test here
-
-# create virtual environment (no harm doing this multiple times)
+# create virtual environment
 python3 -m venv codechat
 
-
-# just make pure install script
-# separate script for running
-# path to codechat server   codechat server start
-# just tell path to binary
-
-# check
+# check if codechat already installed
 if [[ -d codechat ]]; then
-    # echo "CodeChat already installed"
-    codechat/bin/CodeChat_Server serve
+    echo "CodeChat already installed"
+    exit 1
 else
     codechat/bin/python3 -m pip install --upgrade CodeChat_Server
-    codechat/bin/CodeChat_Server serve
 fi
 
 # echo server directory to user
+echo "CodeChat installation complete!"
+echo "Install directory:"
 which CodeChat_Server
 
 
